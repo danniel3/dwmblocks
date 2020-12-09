91b3b718ed1bead256546d2baa691fe40a440d6b
danniel3 <k2kobra@gmail.com>
2020-12-09 14:24:41 +0400

    Removed mail

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
+	/* {"", "cat /tmp/recordingicon 2>/dev/null",	0,	9}, */
 	/* {"",	"music",	0,	11}, */
 	{"",	"pacpackages",	0,	8},
 	{"",	"news",		0,	6},
@@ -16,7 +16,7 @@ static const Block blocks[] = {
 	/* {"",	"memory",	10,	14}, */
 	/* {"",	"cpu",		10,	18}, */
 	/* {"",	"moonphase",	18000,	17}, */
-	{"",	"mailbox",	180,	12},
+	/* {"",	"mailbox",	180,	12}, */
 	{"",	"nettraf",	1,	16},
 	{"",	"forecast",	18000,	5},
 	{"",	"volume",	0,	10},
